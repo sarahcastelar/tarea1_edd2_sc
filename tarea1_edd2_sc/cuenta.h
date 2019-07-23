bbae9534b3d5f370bc703d65b4e95cdc123b4e1f
sarahcastelar <sarahcastelar@unitec.edu>
2019-07-22 19:25:48 -0600

    se agrega los clientes/cuentas

--- tarea1_edd2_sc/cuenta.h
+++ tarea1_edd2_sc/cuenta.h
@@ -6,23 +6,26 @@ using namespace std;
 
 struct cuentas {
 	char id[5];
-	char nombre[5];
+	char nombre[9];
 	float saldo;
 
 	cuentas() {
 		strcpy_s(this->id, "-1");
 		strcpy_s(this->nombre, "-1");
-		saldo = -1;
+		saldo = 0;
 	}
 };
 
 class cuenta
 {
 public:
-	void guardarArchivo(cuentas test) {
-		ofstream file("file.dat", ios::out | ios::binary);
+	void guardarArchivo(cuentas struct_cuentas) {
+		ofstream file("archivoCuentas.dat", ios::out | ios::binary);
 		file.seekp(0, ios::end);
-		file.write(reinterpret_cast<const char*>(&test), sizeof(cuentas));
+		file.write(reinterpret_cast<const char*>(&struct_cuentas), sizeof(cuentas));
+		cout << "se guardo en el archivo: " << endl;
+		cout << struct_cuentas.id << " " << struct_cuentas.nombre << " " << struct_cuentas.saldo << endl;
+		cout << "done" << endl;
 	}
 
 };
--- tarea1_edd2_sc/main.cpp
+++ tarea1_edd2_sc/main.cpp
@@ -6,13 +6,12 @@ using namespace std;
 
 int main() {
 
-	struct cuentas test;
-	strcpy_s(test.nombre, "sara");
-	cout << test.nombre;
+	struct cuentas struct_cuentas;
+	//strcpy_s(struct_cuentas.nombre, "sara");
+	//cout << struct_cuentas.nombre;
 	int resp = -1;
 	char id1[5];
 	char nombre1[5];
-	float saldo;
 	bool hayCuenta = false;
 	cuenta c;
 
@@ -20,7 +19,7 @@ int main() {
 	do {
 		
 		cout << "Bienvenido al Banco" << endl
-			<< "1. Crear cuenta" << endl
+			<< "1. Agregar cliente" << endl
 			<< "2. Depositar dinero" << endl
 			<< "3. Retirar dinero" << endl
 			<< "4. Salir" << endl;
@@ -29,14 +28,15 @@ int main() {
 		if (resp == 1) {
 			cout << "Ingrese id: " << endl;
 			cin >> id1;
-			strcpy_s(test.id, id1);
+			strcpy_s(struct_cuentas.id, id1);
 			cout << "Ingrese nombre: " << endl;
 			cin >> nombre1;
-			strcpy_s(test.nombre, nombre1);
+			strcpy_s(struct_cuentas.nombre, nombre1);
 
-			//guardar el struct en el archivo
-			c.guardarArchivo(test);
+			//guardar el nuevo cliente en el archivo
+			c.guardarArchivo(struct_cuentas);
 			hayCuenta = true;
+			cout << "Cliente/cuenta agregado exitosamente. " << endl;
 		}
 
 		if (resp == 2 && hayCuenta) {
